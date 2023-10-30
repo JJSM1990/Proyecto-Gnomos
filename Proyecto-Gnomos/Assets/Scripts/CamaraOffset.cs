fdfbccf203f411e9f3c879f37c93ae90f61a7668
DESKTOP-KK0LQU3\lelel <lele.lpz03@gmail.com>
2023-10-30 23:56:39 +0100

    camara part 2

--- Proyecto-Gnomos/Assets/Scripts/CamaraOffset.cs
+++ Proyecto-Gnomos/Assets/Scripts/CamaraOffset.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 public class Prueba1 : MonoBehaviour
 {
     [SerializeField] Vector3 _offset = new Vector3(0, 10, -10);
+
     [SerializeField] float _smoothTime = 0.25f;
     private Vector3 _velocity = Vector3.zero;
 
-    private Coroutine _coroutine;
-
-
     [SerializeField] private Transform target;
 
-    [SerializeField] float duration = 1f;
+
+    //[SerializeField] bool start = false;
+    //[SerializeField] AnimationCurve curve;
+
+    //private Coroutine _coroutine;
+
+
+    //[SerializeField] float duration = 1f;
 
 
 
@@ -24,27 +29,44 @@ public class Prueba1 : MonoBehaviour
         Vector3 targetPosition = target.position + _offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, _smoothTime);
 
-        //if(_coroutine == null)
+        //if(start)
         //{
-        //    _coroutine = StartCoroutine(Shaking());
+        //    start = false;
+        //    StartCoroutine(Shaking());
         //}
     }
 
-    IEnumerator Shaking()
-    {
-        Vector3 startPosition = transform.position;
-        Vector3 targetPosition = target.transform.position + Random.insideUnitSphere;
-        targetPosition.y = 5;
-        float currentTime = 0;
+    //IEnumerator Shaking()
+    //{
+    //    Vector3 startPosition = transform.position;
+    //    Vector3 targetPosition = target.transform.position + Random.insideUnitSphere;
+    //    targetPosition.y = 5;
+    //    float currentTime = 0;
 
-        while (currentTime < duration)
-        {
-            Debug.Log("asd");
-            currentTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(startPosition, targetPosition, currentTime/duration);
-            yield return null;
-        }
+    //    while (currentTime < duration)
+    //    {
+    //        Debug.Log("asd");
+    //        currentTime += Time.deltaTime;
+    //        transform.position = Vector3.Lerp(startPosition, targetPosition, currentTime/duration);
+    //        yield return null;
+    //    }
 
-        _coroutine = null;
-    }
+    //    _coroutine = null;
+    //}
+
+    //IEnumerator Shaking()
+    //{
+    //    Vector3 startPosition = transform.position;
+    //    float elapsedTime = 0f;
+
+    //    while (elapsedTime < duration) 
+    //    {
+    //        elapsedTime += Time.deltaTime;
+    //        float strength = curve.Evaluate(elapsedTime / duration);
+    //        transform.position = startPosition + Random.insideUnitSphere * strength;
+    //        yield return null;
+    //    }
+
+    //    transform.position = startPosition;
+    //}
 }

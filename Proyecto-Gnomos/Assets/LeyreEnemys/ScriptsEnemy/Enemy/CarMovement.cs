44d9f84f176256ac4a11cf2194f05786cb453db3
DESKTOP-KK0LQU3\lelel <lele.lpz03@gmail.com>
2023-11-03 14:49:39 +0100

    EnemyCar

--- Proyecto-Gnomos/Assets/LeyreEnemys/ScriptsEnemy/Enemy/CarMovement.cs
+++ Proyecto-Gnomos/Assets/LeyreEnemys/ScriptsEnemy/Enemy/CarMovement.cs
@@ -5,46 +5,56 @@ using UnityEngine.AI;
 
 public class CarMovement : MonoBehaviour
 {
-
-    [SerializeField] NavMeshAgent m_enemy;
-    [SerializeField] EnemyState m_enemyCurrentState;
-    [SerializeField] float m_enemySpeed;
-    private Transform m_target;
-
-    [SerializeField] Transform m_player;
-
-    [SerializeField] Transform[] patrolPoints;
+    [SerializeField] NavMeshAgent m_NavMeshAgent;
+    [SerializeField] Transform[] m_patrolPoints;
     [SerializeField] int _currentPatrolPoint;
 
-    enum EnemyState
+    [SerializeField] float degrees;
+
+    [SerializeField] GameObject m_player;
+    [SerializeField] EnemyState m_currentState;
+
+    private enum EnemyState
     {
         Patrolling,
-        Chasing
+        Chasing,
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        m_enemyCurrentState = EnemyState.Patrolling;
+        _currentPatrolPoint = 0;
+        m_currentState = EnemyState.Patrolling;
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_target = patrolPoints[_currentPatrolPoint];
-
-        if(m_enemyCurrentState == EnemyState.Patrolling)
+        if(m_currentState == EnemyState.Patrolling)
         {
-            if(Vector3.Distance(transform.position, m_target.position) < 0.1f)
+            if (!m_NavMeshAgent.hasPath)
             {
-                m_target = patrolPoints[_currentPatrolPoint++ % patrolPoints.Length];
+                m_NavMeshAgent.SetDestination(m_patrolPoints[_currentPatrolPoint].position);
+                _currentPatrolPoint++;
 
-                m_enemy.SetDestination(m_target.position);
-            }
-            else
-            {
-                transform.position = Vector3.MoveTowards(transform.position, m_target.position, m_enemySpeed * Time.deltaTime);
+                if (_currentPatrolPoint > m_patrolPoints.Length - 1)
+                {
+                    _currentPatrolPoint = 0;
+                }
             }
         }
+
+        if(m_currentState == EnemyState.Chasing)
+        {
+            Vector3 direction = m_player.transform.position - transform.position;
+            if(Mathf.Abs(Vector3.Angle(transform.forward, direction)) < degrees)
+            {
+                m_NavMeshAgent.SetDestination(m_player.transform.position);
+            }
+
+        }
+
     }
+
+
 }

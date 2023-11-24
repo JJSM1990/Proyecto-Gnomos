4a46d49872e6c2219e6f34619bbb4d62bdee27ee
DESKTOP-KK0LQU3\lelel <lele.lpz03@gmail.com>
2023-11-24 13:00:54 +0100

    Menus

--- Proyecto-Gnomos/Assets/Scripts/UI/Scenes.cs
+++ Proyecto-Gnomos/Assets/Scripts/UI/Scenes.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Scenes : MonoBehaviour
 {
     public static readonly string MAIN_MENU_SCENE = "StartMenu";
-    public static readonly string LOADING_SCENE = "Loading";
+    public static readonly string LOADING_SCENE = "Loading_Scene";
     public static readonly string LEVEL_SCENE = "OtherLevel1";
 
 }
